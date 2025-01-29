872398c0bc5e42ee74aa75a4dc74f5c5fb82af08
rotem <rotem@lattica.ai>
2025-01-29 11:42:47 +0200

    change test_decomp_reconstruct.cpp to reuse memory (for the example in the patent)

--- tests/test_decomp_reconstruct.cpp
+++ tests/test_decomp_reconstruct.cpp
@@ -1,34 +1,37 @@
 #include "gtest/gtest.h"
 #include "lattica_hw_api.h"
 
+using namespace lattica_hw_api;
+using IntVec1D = NestedVectorType<int32_t, 1>;
+
+// Define macros for move_to_hardware and allocate_on_hardware
+#define TO_HW(vec) move_to_hardware<int32_t, 1>(vec)
+#define ALLOC_HW(shape) allocate_on_hardware<int32_t>(shape)
+
 TEST(DecompReconstructTests, DecomposeAndReconstruct) {
-    NestedVectorType<int32_t, 1> a_cpu = {51, 29, 63}; // [3]
+    IntVec1D a_cpu = {51, 29, 63}; // [3]
     size_t power = 6;
     size_t base_bits = 1;
 
-    auto a_hw = lattica_hw_api::move_to_hardware<int32_t, 1>(a_cpu);
-    auto decomposition_hw = lattica_hw_api::allocate_on_hardware<int32_t>({a_cpu.size(), power});
+    auto a_hw = TO_HW(a_cpu);
+    auto a_digits_hw = ALLOC_HW((std::vector<size_t>{a_cpu.size(), power}));
 
-    lattica_hw_api::g_decomposition(a_hw, decomposition_hw, power, base_bits);
+    g_decomposition(a_hw, a_digits_hw, power, base_bits);
 
-    NestedVectorType<int32_t, 1> basis_cpu = {1, 2, 4, 8, 16, 32}; // [power]
-    auto basis_hw = lattica_hw_api::move_to_hardware<int32_t, 1>(basis_cpu);
+    auto basis_hw = TO_HW(IntVec1D({1, 2, 4, 8, 16, 32}));
+    auto p6_hw = TO_HW(IntVec1D(6, 1024));
 
-    // Allocate memory for multiplication result
-    auto multiplication_result_hw = lattica_hw_api::allocate_on_hardware<int32_t>({a_cpu.size(), power});
+    // Perform modular multiplication inplace
+    modmul_v2(a_digits_hw, basis_hw, p6_hw, a_digits_hw);
 
-    auto p_hw = lattica_hw_api::move_to_hardware<int32_t, 1>(NestedVectorType<int32_t, 1>(6, 1024));
+    // Reshape with a redundant dimension to obey the axis_modsum API
+    a_digits_hw->reshape({a_cpu.size(), power, 1});
 
-    // Perform modular multiplication
-    lattica_hw_api::modmul_v2(decomposition_hw, basis_hw, p_hw, multiplication_result_hw);
+    auto a_recon_hw = ALLOC_HW((std::vector<size_t>{a_cpu.size(), 1}));
+    auto p1_hw = TO_HW(IntVec1D(1, 1024));
 
-    // Reshape with a redundant dimension to obay the axis_modsum API
-    multiplication_result_hw->reshape({a_cpu.size(), power, 1});
+    axis_modsum(a_digits_hw, p1_hw, a_recon_hw);
 
-    auto reconstruction_hw = lattica_hw_api::allocate_on_hardware<int32_t>({a_cpu.size(), 1});
-    p_hw = lattica_hw_api::move_to_hardware<int32_t, 1>(NestedVectorType<int32_t, 1>(1, 1024)); // Modulo
-    lattica_hw_api::axis_modsum(multiplication_result_hw, p_hw, reconstruction_hw);
-
-    NestedVectorType<int32_t, 1> reconstruction_cpu = lattica_hw_api::move_from_hardware<int32_t, 1>(reconstruction_hw);
+    IntVec1D reconstruction_cpu = move_from_hardware<int32_t, 1>(a_recon_hw);
     ASSERT_EQ(reconstruction_cpu, a_cpu) << "Reconstruction failed.";
 }
