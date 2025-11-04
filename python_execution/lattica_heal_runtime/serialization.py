34807f476ce33ee0d7300a3a2fde7ff932d9de96
liron32 <liron@lattica.ai>
2025-11-04 14:42:58 +0200

    change if else to match case

--- python_execution/lattica_heal_runtime/serialization.py
+++ python_execution/lattica_heal_runtime/serialization.py
@@ -1,6 +1,7 @@
 import json
 import io
 import base64
+from unittest import case
 import numpy as np
 import torch
 
@@ -26,48 +27,57 @@ def heal_json_hook(dct):
     if "__type__" not in dct:
         return dct
 
-    t = dct["__type__"]
+    match dct["__type__"]:
+        case "DeviceOpArgType":
+            return DeviceOpArgType[dct["value"]]
 
-    if t == "DeviceOpArgType":
-        return DeviceOpArgType[dct["value"]]
-    if t == "ExecutionTranscriptOpType":
-        if dct["value"] in ExecutionTranscriptOpType.__members__:
-            return ExecutionTranscriptOpType[dct["value"]]
+        case "ExecutionTranscriptOpType":
+            if dct["value"] in ExecutionTranscriptOpType.__members__:
+                return ExecutionTranscriptOpType[dct["value"]]
 
-    if t == "DeviceTensorPointer":
-        obj = DeviceTensorPointer.__new__(DeviceTensorPointer)
-        obj.dtype = getattr(torch, dct["dtype"].split(".")[-1])
-        obj.inf_name = dct["inf_name"]
-        return obj
-    if t == "HostTensor":
-        obj = HostTensor.__new__(HostTensor)
-        obj.tensor = decode_tensor(dct["tensor_base64"])
-        return obj
-    if t == "DeviceOpArg":
-        arg = DeviceOpArg.__new__(DeviceOpArg)
-        arg.arg_type = dct["arg_type"]
-        arg.value = dct["value"]
-        return arg
-    if t == "DeviceOp":
-        op = DeviceOp.__new__(DeviceOp)
-        op.name = dct["name"]
-        op.args = dct["args"]
-        op.out = dct["out"]
-        return op
-    if t == "FreeDeviceTensor":
-        return FreeDeviceTensor(dct["tensor_name"])
-    if t == "slice":
-        return slice(dct["start"], dct["stop"], dct["step"])
-    if t == "ellipsis":
-        return Ellipsis
-    if t == "ExecutionTranscript":
-        et = ExecutionTranscript()
-        et.transcript = [tuple(item) for item in dct["transcript"]]
-        return et
-    if t == "dtype":
-        return getattr(torch, dct["value"])
-    if t == "complex":
-        return complex(dct["real"], dct["imag"])
+        case "DeviceTensorPointer":
+                obj = DeviceTensorPointer.__new__(DeviceTensorPointer)
+                obj.dtype = getattr(torch, dct["dtype"].split(".")[-1])
+                obj.inf_name = dct["inf_name"]
+                return obj
+
+        case "HostTensor":
+            obj = HostTensor.__new__(HostTensor)
+            obj.tensor = decode_tensor(dct["tensor_base64"])
+            return obj
+
+        case "DeviceOpArg":
+            arg = DeviceOpArg.__new__(DeviceOpArg)
+            arg.arg_type = dct["arg_type"]
+            arg.value = dct["value"]
+            return arg
+
+        case "DeviceOp":
+            op = DeviceOp.__new__(DeviceOp)
+            op.name = dct["name"]
+            op.args = dct["args"]
+            op.out = dct["out"]
+            return op
+
+        case "FreeDeviceTensor":
+            return FreeDeviceTensor(dct["tensor_name"])
+
+        case "slice":
+            return slice(dct["start"], dct["stop"], dct["step"])
+
+        case "ellipsis":
+            return Ellipsis
+
+        case "ExecutionTranscript":
+            et = ExecutionTranscript()
+            et.transcript = [tuple(item) for item in dct["transcript"]]
+            return et
+
+        case "dtype":
+            return getattr(torch, dct["value"])
+
+        case "complex":
+            return complex(dct["real"], dct["imag"])
 
 
 def load_transcript(filename):
