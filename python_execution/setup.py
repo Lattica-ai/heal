acc672394dc38715439e9a13d35244e5b3e35fdd
pavel-mostov <141269147+pavel-mostov@users.noreply.github.com>
2025-12-29 15:07:51 +0200

    Update torch (#52)
    * Liron/update torch version (#51)
    
    * update torch version to 2.9.1
    
    * update min python version to 3.11
    
    ---------
    
    Co-authored-by: liron32 <liron@lattica.ai>
    
    * new version
    
    * update ABI
    
    ---------
    
    Co-authored-by: liron32 <liron@lattica.ai>

--- python_execution/setup.py
+++ python_execution/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="lattica_heal_runtime",
-    version="0.1.0",
+    version="0.1.1",
     author="LatticaAI Inc.",
     author_email="support@lattica.ai",
     packages=find_packages(),
     install_requires=[
-        "torch~=2.5.1",
-        "numpy~=2.0.1",
+        "torch~=2.9.1",
+        "numpy~=2.4.0",
         "pybind11[global]",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.11",
 )
